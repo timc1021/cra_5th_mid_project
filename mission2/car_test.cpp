b0af4048e79b3618962204e5385b8670fd09aed0
timc1021 <iljfyy@gmail.com>
2025-06-09 16:54:51 +0900

    update test
    Signed-off-by: timc1021 <iljfyy@gmail.com>

--- mission2/car_test.cpp
+++ mission2/car_test.cpp
@@ -5,6 +5,8 @@
 
 using namespace testing;
 
+// set functions
+
 TEST(Group, TC1) {
 	Car car;
 
@@ -16,13 +18,113 @@ TEST(Group, TC1) {
 TEST(Group, TC2) {
 	Car car;
 
-	car.setEngine(END_Engine);
+	car.setEngine(TOYOTA);
 
-	int ret = car.run();
+	EXPECT_EQ(car.engine, TOYOTA);
+}
+
+TEST(Group, TC3) {
+	Car car;
+
+	car.setBrakeSystem(MANDO);
+
+	EXPECT_EQ(car.brakeSystem, MANDO);
+}
+
+TEST(Group, TC4) {
+	Car car;
+
+	car.setSteeringSystem(BOSCH_S);
+
+	EXPECT_EQ(car.steeringSystem, BOSCH_S);
+}
+
+// validCheck()
+TEST(validGroup, TC1) {
+	Car car;
+
+	car.setCarType(SEDAN);
+	car.setEngine(GM);
+	car.setBrakeSystem(MANDO);
+	car.setSteeringSystem(MOBIS);
+
+	int ret = car.validCheck(0);
+
+	EXPECT_EQ(ret, true);
+}
+
+TEST(validGroup, TC2) {
+	Car car;
+
+	//SUV + TOYOTA fail
+	car.setCarType(SUV);
+	car.setEngine(TOYOTA);
+	car.setBrakeSystem(BOSCH_B);
+	car.setSteeringSystem(BOSCH_S);
+
+	int ret = car.validCheck(0);
 
 	EXPECT_EQ(ret, false);
 }
 
+TEST(validGroup, TC3) {
+	Car car;
+
+	//TRUCK + WIA fail
+	car.setCarType(TRUCK);
+	car.setEngine(WIA);
+	car.setBrakeSystem(BOSCH_B);
+	car.setSteeringSystem(BOSCH_S);
+
+	int ret = car.validCheck(0);
+
+	EXPECT_EQ(ret, false);
+}
+
+TEST(validGroup, TC4) {
+	Car car;
+
+	//SEDAN + CONTINENTAL fail
+	car.setCarType(SEDAN);
+	car.setEngine(WIA);
+	car.setBrakeSystem(CONTINENTAL);
+	car.setSteeringSystem(BOSCH_S);
+
+	int ret = car.validCheck(0);
+
+	EXPECT_EQ(ret, false);
+}
+
+TEST(validGroup, TC5) {
+	Car car;
+
+	//TRUCK + MANDO fail
+	car.setCarType(TRUCK);
+	car.setEngine(TOYOTA);
+	car.setBrakeSystem(MANDO);
+	car.setSteeringSystem(BOSCH_S);
+
+	int ret = car.validCheck(0);
+
+	EXPECT_EQ(ret, false);
+}
+
+TEST(validGroup, TC6) {
+	Car car;
+
+	//BOSCH_B + non BOSCH fail
+	car.setCarType(SEDAN);
+	car.setEngine(GM);
+	car.setBrakeSystem(BOSCH_B);
+	car.setSteeringSystem(MOBIS);
+
+	int ret = car.validCheck(0);
+
+	EXPECT_EQ(ret, false);
+}
+
+
+// run()
 TEST(combiGroup, TC1) {
 	Car car;
 
@@ -106,6 +208,18 @@ TEST(combiGroup, TC6) {
 	EXPECT_EQ(ret, false);
 }
 
+TEST(combiGroup, TC7) {
+	Car car;
+
+	car.setEngine(END_Engine);
+
+	int ret = car.run();
+
+	EXPECT_EQ(ret, false);
+}
+
+
+// test()
 TEST(combiGroupTestMode, TC1) {
 	Car car;
 
